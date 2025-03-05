f20da8774459a931e2376deef21e59f149465808
eren dağaşan <107785900+erendagasan@users.noreply.github.com>
2025-03-05 09:18:30 +0300

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <div className="">
+        Eren Dağaşan
+      </div>
+    </div>
+  );
+}
